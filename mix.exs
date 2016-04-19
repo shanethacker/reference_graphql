bf8f3fb51c589153bab57940513b294fbc220b39
Shane Thacker <sst@clockworkmind.com>
2016-04-19 16:09:51 -0400

    Added Absinthe

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule ReferenceGraphql.Mixfile do
   def application do
     [mod: {ReferenceGraphql, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :absinthe]]
   end
 
   # Specifies which paths to compile per environment.
@@ -36,7 +36,8 @@ defmodule ReferenceGraphql.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:absinthe, "~> 1.1.0"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
